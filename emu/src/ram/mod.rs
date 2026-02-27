c41087c5c589620eb094f2bf216228f0d24d7fbf
Joshua Seligman <joshuaseligman18@gmail.com>
2026-02-26 21:07:38 -0500

    Initial commit with RAM code

---
+++ emu/src/ram/mod.rs
@@ -0,0 +1,45 @@
+use num_traits::{sign::Unsigned, Bounded, PrimInt};
+
+pub trait WordSize: Unsigned + Bounded + PrimInt {}
+impl WordSize for u8 {}
+
+pub struct Ram<RamWordSize: WordSize> {
+    arr: Vec<RamWordSize>,
+}
+
+impl<RamWordSize: WordSize> Ram<RamWordSize> {
+    pub fn new(addr_space: usize) -> Self {
+        Self {
+            arr: vec![RamWordSize::zero(); addr_space],
+        }
+    }
+
+    pub fn read(&self, addr: usize) -> RamWordSize {
+        self.arr[addr]
+    }
+
+    pub fn write(&mut self, addr: usize, data: RamWordSize) {
+        self.arr[addr] = data;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem::size_of_val;
+
+    #[test]
+    fn test_create_ram() {
+        let my_ram: Ram<u8> = Ram::new(0x10000);
+        assert_eq!(my_ram.arr.len(), 0x10000);
+        assert_eq!(size_of_val(&my_ram.read(0x0000)), 1);
+        assert_eq!(my_ram.read(0x0000), 0);
+    }
+
+    #[test]
+    fn test_write_ram() {
+        let mut my_ram: Ram<u8> = Ram::new(0x10000);
+        my_ram.write(0x4242, 0x07);
+        assert_eq!(my_ram.read(0x4242), 0x07)
+    }
+}
